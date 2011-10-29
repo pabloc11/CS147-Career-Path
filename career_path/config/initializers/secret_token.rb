0d866d34fc7385ab3209b9fff844ba8913583bdd
Pablo Ceballos <pabloc11@stanford.edu>
2011-10-29 14:21:00 -0700

    added default rails project

---
+++ career_path/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CareerPath::Application.config.secret_token = '68645b2245e5b015fd7820f904e84e5efc9307b6eff70e611ac4b7e4e38a4dbff4565f3de87da713409cc4cc03aaa898521a7be1d731b708518934c63dfe4186'
